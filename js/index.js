781d786ac4c9bb7cbb754e1c1903b3218bb84cdb
Anna Hladka <namikosamahime@gmail.com>
2020-02-16 01:42:21 +0200

    rotating cards added

---
+++ js/index.js
@@ -0,0 +1,622 @@
+let pizzaArr = [
+
+  {
+    src: "img/picca-ohotnichya-xxl_1542267870257.jpeg",
+    name: "Пицца Охотничья XXL",
+    ingredients: "Охотничьи колбаски, молочные колбаски, сыр моцарелла, вешенки, шампиньоны, сладкий лук, перец болгарский, соус BBQ, соус маринара, петрушка",
+    price: "289 грн",
+  },
+
+  {
+    src: "img/picca-ohotnichya_1542267892836.jpeg",
+    name: "Пицца Охотничья",
+    ingredients: "Охотничьи колбаски, молочные колбаски, сыр моцарелла, вешенки, шампиньоны, сладкий лук, перец болгарский, соус BBQ, соус маринара, петрушка",
+    price: "129 грн"
+  },
+
+  {
+    src: "img/karbonara-metrovaya_1566297127558.jpeg",
+    name: "Карбонара метровая",
+    ingredients: "Сыр моцарелла, ветчина, грудинка, шампиньоны, пармезан, помидоры черри",
+    price: "299 грн",
+  },
+
+  {
+    src: "img/amerikano-metrovaya_1566296641461.jpeg",
+    name: "Американо метровая",
+    ingredients: "Соус BBQ, соус маринара, сыр моцарелла, горчица, филе куриное sous-vide,  охотничьи колбаски, пепперони, лук конфитюр, кукуруза, перец болгарский",
+    price: "339 грн",
+  },
+
+  {
+    src: "img/margarita_156706066239.jpeg",
+    name: "Маргарита",
+    ingredients: "Соус маринара, помидоры, сыр моцарелла, соус песто",
+    price: "89 грн",
+  },
+
+  {
+    src: "img/karbonara_1567060589165.jpeg",
+    name: "Карбонара",
+    ingredients: "Сыр моцарелла, ветчина, грудинка, шампиньоны, пармезан, помидоры черри",
+    price: "165 грн",
+  },
+
+  {
+    src: "img/pollo_1567060914824.jpeg",
+    name: "Поло",
+    ingredients: "Сыр моцарелла, соус маринара, соус ВВQ, чеснок, ананас, филе куриное sous-vide, перец болгарский",
+    price: "135 грн",
+  },
+
+  {
+    src: "img/gurmeo_1567060279780.jpeg",
+    name: "Гурмео",
+    ingredients: "Соус BBQ, филе куриное sous-vide, ветчина, охотничьи колбаски, пеперони, сыр моцарелла, шампиньоны, лук репчатый, петрушка",
+    price: "165 грн",
+  },
+
+  {
+    src: "img/chetyre-syra_1567059967719.jpeg",
+    name: "Четыре сыра",
+    ingredients: "Сливочный соус, сыр моцарелла, сыр дор-блю, сыр чеддер, груша, сыр пармезан, орех грецкий",
+    price: "149 грн",
+  },
+
+  {
+    src: "img/amerikano_1567060058936.jpeg",
+    name: "Американо",
+    ingredients: "Соус BBQ, соус маринара, сыр моцарелла, горчица, филе куриное sous-vide, охотничьи колбаски, пепперони, конфитюр из лука, кукуруза, перец болгарский",
+    price: "149 грн",
+  },
+
+  {
+    src: "img/kalcone_1567060522194.jpeg",
+    name: "Кальцоне",
+    ingredients: "Соус маринара, сыр моцарелла, шампиньоны, сыр дор-блю, помидоры, ветчина, соус песто",
+    price: "119 грн",
+  },
+
+  {
+    src: "img/bavarskaya_1567060134436.jpeg",
+    name: "Баварская",
+    ingredients: "Сыр моцарелла, соус маринара, колбаски мюнхенские, охотничьи колбаски, огурец соленый, горчица, лук репчатый, лук зеленый",
+    price: "145 грн",
+  },
+
+  {
+    src: "img/diablo_1567060435236.jpeg",
+    name: "Диабло",
+    ingredients: "Сыр моцарелла, пепперони, перец чили, перец болгарский, лук репчатый, соус ВВQ,соус чили, петрушка",
+    price: "159 грн",
+  },
+
+  {
+    src: "img/ovoshchnaya_1567060751874.jpeg",
+    name: "Овощная",
+    ingredients: "Баклажан, вешенки, перец болгарский, помидоры, соус песто, соус маринара",
+    price: "95 грн",
+  },
+
+  {
+    src: "img/bavarskaya-metrovaya_156629673225.jpeg",
+    name: "Баварская метровая",
+    ingredients: "Сыр моцарелла, соус маринара, колбаски мюнхенские, охотничьи колбаски, огурец соленый, горчица, лук репчатый, лук зеленый",
+    price: "319 грн",
+  },
+
+  {
+    src: "img/berluskoni-metrovaya_1566296807287.jpeg",
+    name: "Берлускони метровая",
+    ingredients: "Сливочный соус из белых грибов, сыр моцарелла, сыр дор-блю, пармезан, шампиньоны, вешенки, соус чесночный, масло трюфельное, петрушка",
+    price: "299 грн",
+  },
+
+  {
+    src: "img/gurmeo-metrovaya_1566296981221.jpeg",
+    name: "Гурмео метровая",
+    ingredients: "Соус BBQ, филе куриное sous-vide, ветчина, охотничьи колбаски, пепперони, сыр моцарелла, шампиньоны, лук репчатый, петрушка",
+    price: "329 грн",
+  },
+
+  {
+    src: "img/diablo-metrovaya_1566297055552.jpeg",
+    name: "Диабло метровая",
+    ingredients: "Сыр моцарелла, пепперони, перец чили, перец болгарский, лук репчатый, соус ВВQ, соус шрирачи, петрушка",
+    price: "319 грн",
+  },
+
+  {
+    src: "img/polo-metrovaya_1566297194328.jpeg",
+    name: "Поло метровая",
+    ingredients: "Сыр моцарелла, соус маринара, соус BBQ, чеснок, ананас, филе куриное sous-vide, перец болгарский",
+    price: "289 грн",
+  },
+
+  {
+    src: "img/berluskoni_1567060209678.jpeg",
+    name: "Берлускони",
+    ingredients: "Сливочный соус из белых грибов, сыр моцарелла, сыр дор-блю, пармезан, шампиньоны, вешенки, соус чесночный, масло трюфельное, петрушка",
+    price: "149 грн",
+  },
+
+  {
+    src: "img/picca-ot-shefa_1567060849842.jpeg",
+    name: "Пицца от шефа",
+    ingredients: "Говядина sous-vide, черный камамбер, шампиньоны, помидоры, болгарский перец, руккола, пармезан, сыр моцарелла, каперсы, соус маринара",
+    price: "159 грн",
+  },
+]
+
+let filterClicked = false;
+
+let view;
+let mainCont = document.querySelector(".main-content");
+let container = createteElWithClass("div", "container");
+
+function fixIngridients() {
+  for (let i = 0; i < pizzaArr.length; i++) {
+    let ingrs = pizzaArr[i].ingredients.split(",");
+    for (let j = 0; j < ingrs.length; j++) {
+      ingrs[j] = ingrs[j].trim();
+    }
+    pizzaArr[i].ingredients = ingrs;
+  }
+}
+fixIngridients();
+
+function addRandomCalories() {
+  for (let i = 0; i < pizzaArr.length; i++) {
+    let calories = 900 + Math.floor(Math.random() * 400);
+    if (pizzaArr[i].ingredients.includes("колбаски") || pizzaArr[i].ingredients.includes("ветчина")) {
+      calories += Math.floor(Math.random() * 300);
+    }
+    if (pizzaArr[i].name.includes("метровая") || pizzaArr[i].name.includes("XXL")) {
+      calories += 800 + Math.floor(Math.random() * 300);
+    }
+    pizzaArr[i].calories = calories;
+  }
+}
+addRandomCalories();
+
+let pizzaArrCopy = [];
+for (let i = 0; i < pizzaArr.length; i++) {
+  let copy = Object.assign({}, pizzaArr[i]);
+  pizzaArrCopy.push(copy);
+}
+
+let arrIngr = [];
+let elsForFiltering = [];
+for (let i = 0; i < pizzaArr.length; i++) {
+  for (let j = 0; j < pizzaArr[i].ingredients.length; j++) {
+    arrIngr.push(pizzaArr[i].ingredients[j].toLocaleLowerCase());
+  }
+}
+console.log(arrIngr);
+if (arrIngr != 0) {
+  for (let i = 0; i < arrIngr.length; i++) {
+    let isUnique = true;
+    for (let k = 0; k < elsForFiltering.length; k++) {
+      if (arrIngr[i] == elsForFiltering[k]) {
+        isUnique = false;
+        break;
+      }
+    }
+    if (isUnique) {
+      elsForFiltering.push(arrIngr[i]);
+    }
+  }
+}
+console.log(elsForFiltering);
+
+// Modal logic
+
+function createteElWithClass(el, cls) {
+  let elem = document.createElement(el);
+  elem.className = cls;
+  return elem;
+}
+
+function viewAsker() {
+  let viewAskerBlock = createteElWithClass("div", "viewasker");
+  let block = document.createElement("div");
+  let a = createteElWithClass("a", "logo");
+  a.innerText = "AwesomePizza";
+  let btnHolder = createteElWithClass("div", "btn-holder");
+  let btn1 = createteElWithClass("button", "btn");
+  btn1.id = "view-grid";
+  btn1.innerText = "Сетка";
+  let btn2 = createteElWithClass("button", "btn");
+  btn2.id = "view-list";
+  btn2.innerText = "Список";
+  btnHolder.append(btn1, btn2);
+  block.append(a, btnHolder);
+  viewAskerBlock.append(block);
+  document.body.prepend(viewAskerBlock);
+  document.body.classList.add("unactive");
+
+  btn1.addEventListener("click", function () {
+    view = "grid";
+    viewAskerBlock.remove();
+    document.body.classList.remove("unactive");
+    generateMainContent();
+  });
+
+  btn2.addEventListener("click", function () {
+    view = "list";
+    viewAskerBlock.remove();
+    document.body.classList.remove("unactive");
+    generateMainContent();
+  })
+
+}
+viewAsker();
+
+
+
+
+
+
+
+//Grid
+function generateProdViewSwitcher() {
+  let prodViewSwitch = createteElWithClass("div", "product-view-swtcher");
+  let btnGrid = createteElWithClass("button", "btn");
+  btnGrid.setAttribute("id", "btnGrid");
+  let iconGrid = createteElWithClass("i", "fas fa-th-large");
+  btnGrid.append(iconGrid);
+
+  let btnList = createteElWithClass("button", "btn");
+  btnList.setAttribute("id", "btnList");
+  let iconList = createteElWithClass("i", "fas fa-list");
+  btnList.append(iconList);
+
+  prodViewSwitch.append(btnGrid, btnList);
+  container.appendChild(prodViewSwitch);
+  if (view === "grid") {
+    btnGrid.classList.add("active");
+  }
+  if (view === "list") {
+    btnList.classList.add("active");
+  }
+}
+
+function generateMainContent() {
+  generateProdViewSwitcher();
+  generateView(pizzaArr);
+  switchViewBtns();
+}
+
+
+function generateView(arr) {
+  let prodView = createteElWithClass("div", "product-view");
+  switch (view) {
+    case "grid":
+      let ViewRow = createteElWithClass("div", "row");
+      prodView.classList.toggle("product-grid");
+      ViewRow.append(generateFilterBlock(), generateGridGoods(arr));
+      prodView.append(ViewRow);
+      container.appendChild(prodView);
+      mainCont.appendChild(container);
+      filterListeners();
+      break;
+    case "list":
+      prodView.classList.toggle("product-list");
+      generateSelectSort();
+      prodView.appendChild(generateListGoods(arr));
+      container.appendChild(prodView);
+      mainCont.appendChild(container);
+      sortingListeners();
+      break;
+  }
+
+
+}
+
+function switchViewBtns() {
+  let btnGrid = document.getElementById("btnGrid");
+  let btnList = document.getElementById("btnList");
+  btnGrid.addEventListener("click", function () {
+    if (view === "list") {
+      btnGrid.classList.add("active");
+      btnList.classList.remove("active");
+      document.querySelector(".select-wrap").remove();
+      document.querySelector(".product-view").remove();
+      view = "grid";
+      generateView(pizzaArr);
+    }
+  });
+  btnList.addEventListener("click", function () {
+    if (view === "grid") {
+      btnList.classList.add("active");
+      btnGrid.classList.remove("active");
+      document.querySelector(".product-view").remove();
+      view = "list";
+      generateView(pizzaArr);
+    }
+
+  });
+}
+
+
+function generateFilterBlock() {
+  let filterCol = createteElWithClass("div", "col-3");
+  let filter = createteElWithClass("div", "prod-filter");
+  let filterP = document.createElement("p");
+  filterP.innerText = "Фильтр";
+  let filterForm = createteElWithClass("form", "filter-form");
+  for (i = 0; i < elsForFiltering.length; i++) {
+    let legend = document.createElement("legend");
+    let label = document.createElement("label");
+    let input = document.createElement("input");
+    label.setAttribute("for", "ingr" + (i + 1));
+    label.innerText = elsForFiltering[i];
+    input.id = "ingr" + (i + 1);
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("value", elsForFiltering[i]);
+    legend.append(input, label);
+    filterForm.appendChild(legend);
+  }
+  let btnClear = createteElWithClass("button", "btn");
+  btnClear.setAttribute("type", "button");
+  btnClear.id = "btnClear";
+  btnClear.innerText = "Очистить";
+  let btnApply = createteElWithClass("button", "btn");
+  btnApply.setAttribute("type", "button");
+  btnApply.id = "btnApply";
+  btnApply.innerText = "Применить";
+  filterForm.append(btnClear, btnApply);
+  filter.append(filterP, filterForm);
+  filterCol.appendChild(filter);
+  return filterCol;
+}
+function generateGridGoods(arr) {
+  let ViewCol = createteElWithClass("div", "col-9");
+  ViewCol.id = "prod-view"
+  let row = createteElWithClass("div", "row");
+
+
+  for (let i = 0; i < arr.length; i++) {
+    let col = createteElWithClass("div", "col-6");
+    if (i % 2 === 0) {
+      row = createteElWithClass("div", "row");
+    }
+
+    let prodHolder = createteElWithClass("div", "prod-holder");
+    let prodImg = createteElWithClass("div", "prod-img");
+    let img = document.createElement("img");
+    img.src = arr[i].src;
+    prodImg.appendChild(img);
+    let prodProp = createteElWithClass("div", "prod-prop");
+    let prodName = createteElWithClass("div", "prod-name");
+    let prodNameA = document.createElement("a");
+    prodNameA.setAttribute("href", "#");
+    prodNameA.textContent = arr[i].name;
+    prodName.appendChild(prodNameA);
+    let prodKkal = createteElWithClass("div", "prod-kkal");
+    prodKkal.textContent = arr[i].calories + " кал.";
+    let prodDescr = createteElWithClass("div", "prod-descr");
+
+    for (let j = 0; j < arr[i].ingredients.length; j++) {
+      let prodIngr = createteElWithClass("span", "ingr");
+      prodIngr.innerHTML = arr[i].ingredients[j] + ", ";
+      if (j === arr[i].ingredients.length - 1) {
+        prodIngr.innerHTML = arr[i].ingredients[j];
+      }
+      prodDescr.append(prodIngr);
+    }
+
+    let prodPrice = createteElWithClass("div", "prod-price");
+    prodPrice.innerText = arr[i].price;
+    prodProp.append(prodName, prodDescr, prodKkal, prodPrice);
+    prodHolder.append(prodProp, prodImg);
+    col.appendChild(prodHolder);
+    row.appendChild(col);
+    ViewCol.append(row);
+
+
+    // prodName.addEventListener("click", function () {
+    //   let parent = this.parentElement;
+    //   parent.parentElement.classList.add("rotate");
+    // });
+
+    // prodImg.addEventListener("click", function () {
+    //   let parent = this.parentElement;
+    //   parent.classList.remove("rotate");
+    // });
+
+    prodHolder.onclick = function (event) {
+      let span = event.target.closest("span");
+      let name = event.target.closest(".prod-name");
+      let image = event.target.closest(".prod-img");
+      if (span) {
+        span.classList.toggle("linethrough");
+        console.log(span.innerHTML);
+      }
+      if (name) {
+        this.classList.add("rotate");
+      }
+      if (image) {
+        this.classList.remove("rotate");
+      }
+    }
+
+
+  }
+  return ViewCol;
+}
+
+function generateSelectSort() {
+  let selectWrap = createteElWithClass("div", "select-wrap");
+  let selectHolder = createteElWithClass("div", "select-holder");
+  let select = document.createElement("select");
+  select.id = "sorting";
+  let optDef = document.createElement("option");
+  optDef.setAttribute("value", "default");
+  optDef.innerText = "По умолчанию";
+  optDef.selected = true;
+  let optPriceFromLow = document.createElement("option");
+  optPriceFromLow.setAttribute("value", "priceFromLow");
+  optPriceFromLow.innerText = "По возрастанию цены";
+  let optPriceFromHigh = document.createElement("option");
+  optPriceFromHigh.setAttribute("value", "priceFromHigh");
+  optPriceFromHigh.innerText = "По уменьшению цены";
+  let optNameAlphabet = document.createElement("option");
+  optNameAlphabet.setAttribute("value", "nameAlphabet");
+  optNameAlphabet.innerText = "По алфавиту";
+  select.append(optDef, optPriceFromLow, optPriceFromHigh, optNameAlphabet);
+  selectHolder.appendChild(select);
+  selectWrap.appendChild(selectHolder);
+  container.append(selectWrap);
+}
+function generateListGoods(arr) {
+  let prodViewHolder = document.createElement("div");
+  prodViewHolder.id = "prod-view";
+  for (let i = 0; i < arr.length; i++) {
+    let prodHolder = createteElWithClass("div", "prod-holder");
+    let prodImg = createteElWithClass("div", "prod-img");
+    let img = document.createElement("img");
+    img.src = "img/pizza-ic.png";
+    prodImg.appendChild(img);
+    let prodProp = createteElWithClass("div", "prod-prop");
+    let prodName = createteElWithClass("div", "prod-name");
+    let prodNameA = document.createElement("a");
+    prodNameA.setAttribute("href", "#");
+    prodNameA.textContent = arr[i].name;
+    prodName.appendChild(prodNameA);
+    let prodPrice = createteElWithClass("div", "prod-price");
+    prodPrice.innerText = arr[i].price;
+    prodProp.append(prodName, prodPrice)
+    prodHolder.append(prodImg, prodProp);
+    prodViewHolder.appendChild(prodHolder);
+  }
+  return prodViewHolder;
+}
+
+let filteredArr;
+function filterListeners() {
+  let btnApply = document.getElementById("btnApply");
+  let btnClear = document.getElementById("btnClear");
+  let filterForm = document.querySelector(".filter-form");
+  let inputs = filterForm.getElementsByTagName("input");
+  let checkedInputs = [];
+
+  function findCheckedInputs() {
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].checked) {
+        let checkedInput = inputs[i].value;
+        checkedInputs.push(checkedInput);
+      }
+    }
+    if (checkedInputs.length == 0) {
+      alert("Выберете хотя бы одно из значений");
+    }
+  }
+
+
+  function filterArr(arr) {
+    filteredArr = arr.filter(function (pizza) {
+      let result;
+      for (i = 0; i < checkedInputs.length; i++) {
+        let findEl = checkedInputs[i];
+        for (k = 0; k < pizza.ingredients.length; k++) {
+          let pizzaIngr = pizza.ingredients[k].toLocaleLowerCase();
+          result = (findEl === pizzaIngr);
+          if (result) {
+            break;
+          }
+        }
+        return result;
+      }
+
+
+    });
+
+  }
+
+  btnApply.addEventListener("click", function () {
+    findCheckedInputs();
+
+    if (checkedInputs.length != 0) {
+      filterArr(pizzaArr);
+      let prodView = document.getElementById("prod-view");
+      prodView.remove();
+      let targetRow = document.querySelector(".product-grid > .row");
+      targetRow.appendChild(generateGridGoods(filteredArr));
+      filterClicked = true;
+      checkedInputs = [];
+      filteredArr = [];
+    }
+
+  });
+  btnClear.addEventListener("click", function () {
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].checked) {
+        inputs[i].checked = false;
+      }
+    }
+    filterClicked = false;
+    checkedInputs = [];
+    filteredArr = [];
+    let prodView = document.getElementById("prod-view");
+    prodView.remove();
+    let targetRow = document.querySelector(".product-grid > .row");
+    targetRow.appendChild(generateGridGoods(pizzaArr));
+  });
+
+}
+
+
+function comparePriceFromLow(a, b) {
+  a = parseInt(a.price);
+  b = parseInt(b.price);
+  return a - b;
+}
+function comparePriceFromHigh(a, b) {
+  a = parseInt(a.price);
+  b = parseInt(b.price);
+  return b - a;
+}
+function compareName(a, b) {
+  a = a.name;
+  b = b.name;
+  if (a < b) { return -1; }
+  if (a > b) { return 1; }
+  return 0;
+}
+
+function sortingListeners() {
+  let select = document.getElementById("sorting");
+  select.addEventListener("change", function () {
+    let prodView = document.getElementById("prod-view");
+    let targetRow = document.querySelector(".product-list");
+
+    switch (this.value) {
+      case "priceFromLow":
+        prodView.remove();
+        pizzaArr.sort(comparePriceFromLow);
+        targetRow.appendChild(generateListGoods(pizzaArr));
+        break;
+
+      case "priceFromHigh":
+        prodView.remove();
+        pizzaArr.sort(comparePriceFromHigh);
+        targetRow.appendChild(generateListGoods(pizzaArr));
+        break;
+
+      case "nameAlphabet":
+        prodView.remove();
+        pizzaArr.sort(compareName);
+        targetRow.appendChild(generateListGoods(pizzaArr));
+        break;
+
+      case "default":
+        prodView.remove();
+        targetRow.appendChild(generateListGoods(pizzaArrCopy));
+        break;
+    }
+  });
+}
+
+
+
