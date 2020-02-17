e07be71d394923a6d911351a48f25702eb9d70ee
Anna Hladka <namikosamahime@gmail.com>
2020-02-18 01:58:51 +0200

    edit prices added

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ let pizzaArr = [
   {
     src: "img/karbonara-metrovaya_1566297127558.jpeg",
     name: "Карбонара метровая",
-    ingredients: "Сыр моцарелла, ветчина, грудинка, шампиньоны, пармезан, помидоры черри",
+    ingredients: "Сыр моцарелла, ветчина, грудинка, шампиньоны, сыр пармезан, помидоры черри",
     price: "299 грн",
   },
 
@@ -38,21 +38,21 @@ let pizzaArr = [
   {
     src: "img/karbonara_1567060589165.jpeg",
     name: "Карбонара",
-    ingredients: "Сыр моцарелла, ветчина, грудинка, шампиньоны, пармезан, помидоры черри",
+    ingredients: "Сыр моцарелла, ветчина, грудинка, шампиньоны, сыр пармезан, помидоры черри",
     price: "165 грн",
   },
 
   {
     src: "img/pollo_1567060914824.jpeg",
     name: "Поло",
-    ingredients: "Сыр моцарелла, соус маринара, соус ВВQ, чеснок, ананас, филе куриное sous-vide, перец болгарский",
+    ingredients: "Сыр моцарелла, соус маринара, соус BBQ, чеснок, ананас, филе куриное sous-vide, перец болгарский",
     price: "135 грн",
   },
 
   {
     src: "img/gurmeo_1567060279780.jpeg",
     name: "Гурмео",
-    ingredients: "Соус BBQ, филе куриное sous-vide, ветчина, охотничьи колбаски, пеперони, сыр моцарелла, шампиньоны, лук репчатый, петрушка",
+    ingredients: "Соус BBQ, филе куриное sous-vide, ветчина, охотничьи колбаски, пепперони, сыр моцарелла, шампиньоны, лук репчатый, петрушка",
     price: "165 грн",
   },
 
@@ -66,7 +66,7 @@ let pizzaArr = [
   {
     src: "img/amerikano_1567060058936.jpeg",
     name: "Американо",
-    ingredients: "Соус BBQ, соус маринара, сыр моцарелла, горчица, филе куриное sous-vide, охотничьи колбаски, пепперони, конфитюр из лука, кукуруза, перец болгарский",
+    ingredients: "Соус BBQ, соус маринара, сыр моцарелла, горчица, филе куриное sous-vide, охотничьи колбаски, пепперони, лук конфитюр, кукуруза, перец болгарский",
     price: "149 грн",
   },
 
@@ -87,7 +87,7 @@ let pizzaArr = [
   {
     src: "img/diablo_1567060435236.jpeg",
     name: "Диабло",
-    ingredients: "Сыр моцарелла, пепперони, перец чили, перец болгарский, лук репчатый, соус ВВQ,соус чили, петрушка",
+    ingredients: "Сыр моцарелла, пепперони, перец чили, перец болгарский, лук репчатый, соус BBQ, соус чили, петрушка",
     price: "159 грн",
   },
 
@@ -108,7 +108,7 @@ let pizzaArr = [
   {
     src: "img/berluskoni-metrovaya_1566296807287.jpeg",
     name: "Берлускони метровая",
-    ingredients: "Сливочный соус из белых грибов, сыр моцарелла, сыр дор-блю, пармезан, шампиньоны, вешенки, соус чесночный, масло трюфельное, петрушка",
+    ingredients: "Сливочный соус из белых грибов, сыр моцарелла, сыр дор-блю, сыр пармезан, шампиньоны, вешенки, соус чесночный, масло трюфельное, петрушка",
     price: "299 грн",
   },
 
@@ -122,7 +122,7 @@ let pizzaArr = [
   {
     src: "img/diablo-metrovaya_1566297055552.jpeg",
     name: "Диабло метровая",
-    ingredients: "Сыр моцарелла, пепперони, перец чили, перец болгарский, лук репчатый, соус ВВQ, соус шрирачи, петрушка",
+    ingredients: "Сыр моцарелла, пепперони, перец чили, перец болгарский, лук репчатый, соус BBQ, соус шрирачи, петрушка",
     price: "319 грн",
   },
 
@@ -136,14 +136,14 @@ let pizzaArr = [
   {
     src: "img/berluskoni_1567060209678.jpeg",
     name: "Берлускони",
-    ingredients: "Сливочный соус из белых грибов, сыр моцарелла, сыр дор-блю, пармезан, шампиньоны, вешенки, соус чесночный, масло трюфельное, петрушка",
+    ingredients: "Сливочный соус из белых грибов, сыр моцарелла, сыр дор-блю, сыр пармезан, шампиньоны, вешенки, соус чесночный, масло трюфельное, петрушка",
     price: "149 грн",
   },
 
   {
     src: "img/picca-ot-shefa_1567060849842.jpeg",
     name: "Пицца от шефа",
-    ingredients: "Говядина sous-vide, черный камамбер, шампиньоны, помидоры, болгарский перец, руккола, пармезан, сыр моцарелла, каперсы, соус маринара",
+    ingredients: "Говядина sous-vide, черный камамбер, шампиньоны, помидоры, перец болгарский, руккола, сыр пармезан, сыр моцарелла, каперсы, соус маринара",
     price: "159 грн",
   },
 ]
@@ -165,19 +165,99 @@ function fixIngridients() {
 }
 fixIngridients();
 
-function addRandomCalories() {
+let ingrsPriceAndCal = new Map([
+  ["охотничьи колбаски", { price: 25, cal: 302 }],
+  ["молочные колбаски", { price: 25, cal: 308 }],
+  ["сыр моцарелла", { price: 22, cal: 100 }],
+  ["вешенки", { price: 15, cal: 90 }],
+  ["шампиньоны", { price: 15, cal: 85 }],
+  ["сладкий лук", { price: 13, cal: 70 }],
+  ["перец болгарский", { price: 17, cal: 110 }],
+  ["соус bbq", { price: 19, cal: 80 }],
+  ["соус маринара", { price: 24, cal: 90 }],
+  ["петрушка", { price: 4, cal: 20 }],
+  ["ветчина", { price: 26, cal: 200 }],
+  ["грудинка", { price: 35, cal: 289 }],
+  ["сыр пармезан", { price: 20, cal: 100 }],
+  ["помидоры черри", { price: 25, cal: 155 }],
+  ["горчица", { price: 10, cal: 50 }],
+  ["филе куриное sous-vide", { price: 35, cal: 300 }],
+  ["пепперони", { price: 25, cal: 240 }],
+  ["лук конфитюр", { price: 15, cal: 100 }],
+  ["кукуруза", { price: 15, cal: 130 }],
+  ["помидоры", { price: 22, cal: 169 }],
+  ["соус песто", { price: 20, cal: 200 }],
+  ["чеснок", { price: 12, cal: 100 }],
+  ["ананас", { price: 20, cal: 110 }],
+  ["лук репчатый", { price: 8, cal: 100 }],
+  ["сливочный соус", { price: 15, cal: 120 }],
+  ["сыр дор - блю", { price: 30, cal: 150 }],
+  ["сыр чеддер", { price: 28, cal: 120 }],
+  ["груша", { price: 15, cal: 105 }],
+  ["орех грецкий", { price: 15, cal: 100 }],
+  ["колбаски мюнхенские", { price: 27, cal: 245 }],
+  ["огурец соленый", { price: 15, cal: 105 }],
+  ["лук зеленый", { price: 8, cal: 50 }],
+  ["перец чили", { price: 15, cal: 87 }],
+  ["соус чили", { price: 17, cal: 99 }],
+  ["баклажан", { price: 15, cal: 110 }],
+  ["сливочный соус из белых грибов", { price: 20, cal: 110 }],
+  ["соус чесночный", { price: 15, cal: 53 }],
+  ["масло трюфельное", { price: 13, cal: 109 }],
+  ["соус шрирачи", { price: 18, cal: 90 }],
+  ["говядина sous-vide", { price: 35, cal: 370 }],
+  ["черный камамбер", { price: 25, cal: 114 }],
+  ["руккола", { price: 15, cal: 99 }],
+  ["каперсы", { price: 15, cal: 90 }],
+]);
+
+let pizzaPrices = [];
+let pizzaCals = [];
+
+function calcPriceAndCal() {
   for (let i = 0; i < pizzaArr.length; i++) {
-    let calories = 900 + Math.floor(Math.random() * 400);
-    if (pizzaArr[i].ingredients.includes("колбаски") || pizzaArr[i].ingredients.includes("ветчина")) {
-      calories += Math.floor(Math.random() * 300);
+    let price = 0;
+    let cal = 0;
+    for (let j = 0; j < pizzaArr[i].ingredients.length; j++) {
+
+      let pizIngr = pizzaArr[i].ingredients[j].toLocaleLowerCase();
+
+      ingrsPriceAndCal.forEach(function (val, key) {
+        let pizIngrPrice = val.price;
+        let pizCal = val.cal;
+        if (pizIngr == key) {
+
+          price += pizIngrPrice;
+          cal += pizCal;
+        }
+
+      })
+
     }
+
+
     if (pizzaArr[i].name.includes("метровая") || pizzaArr[i].name.includes("XXL")) {
-      calories += 800 + Math.floor(Math.random() * 300);
+      price *= 2;
+      cal *= 2;
+
     }
-    pizzaArr[i].calories = calories;
+    pizzaPrices.push(price);
+    pizzaCals.push(cal);
   }
 }
-addRandomCalories();
+
+calcPriceAndCal();
+
+
+function setPriceAndCal() {
+  for (let i = 0; i < pizzaArr.length; i++) {
+
+    pizzaArr[i].price = pizzaPrices[i];
+    pizzaArr[i].calories = pizzaCals[i];
+
+  }
+}
+setPriceAndCal();
 
 let pizzaArrCopy = [];
 for (let i = 0; i < pizzaArr.length; i++) {
@@ -209,13 +289,16 @@ if (arrIngr != 0) {
 }
 console.log(elsForFiltering);
 
+
 // Modal logic
+
 
 function createteElWithClass(el, cls) {
   let elem = document.createElement(el);
   elem.className = cls;
   return elem;
 }
+
 
 function viewAsker() {
   let viewAskerBlock = createteElWithClass("div", "viewasker");
@@ -248,14 +331,8 @@ function viewAsker() {
     document.body.classList.remove("unactive");
     generateMainContent();
   })
-
 }
 viewAsker();
-
-
-
-
-
 
 
 //Grid
@@ -281,6 +358,7 @@ function generateProdViewSwitcher() {
   }
 }
 
+
 function generateMainContent() {
   generateProdViewSwitcher();
   generateView(pizzaArr);
@@ -299,6 +377,8 @@ function generateView(arr) {
       container.appendChild(prodView);
       mainCont.appendChild(container);
       filterListeners();
+      rotateProd();
+      editBtnListener();
       break;
     case "list":
       prodView.classList.toggle("product-list");
@@ -310,8 +390,8 @@ function generateView(arr) {
       break;
   }
 
-
 }
+
 
 function switchViewBtns() {
   let btnGrid = document.getElementById("btnGrid");
@@ -370,14 +450,15 @@ function generateFilterBlock() {
   filterCol.appendChild(filter);
   return filterCol;
 }
+
+
 function generateGridGoods(arr) {
   let ViewCol = createteElWithClass("div", "col-9");
   ViewCol.id = "prod-view"
   let row = createteElWithClass("div", "row");
 
-
   for (let i = 0; i < arr.length; i++) {
-    let col = createteElWithClass("div", "col-6");
+    let col = createteElWithClass("div", "col-6 even-height");
     if (i % 2 === 0) {
       row = createteElWithClass("div", "row");
     }
@@ -389,62 +470,175 @@ function generateGridGoods(arr) {
     prodImg.appendChild(img);
     let prodProp = createteElWithClass("div", "prod-prop");
     let prodName = createteElWithClass("div", "prod-name");
+    let editBtn = createteElWithClass("button", "btn btn-edit");
+    let editicon = createteElWithClass("i", "fas fa-pencil-alt");
+    editBtn.appendChild(editicon);
     let prodNameA = document.createElement("a");
     prodNameA.setAttribute("href", "#");
     prodNameA.textContent = arr[i].name;
     prodName.appendChild(prodNameA);
-    let prodKkal = createteElWithClass("div", "prod-kkal");
-    prodKkal.textContent = arr[i].calories + " кал.";
+    let addIngr = createteElWithClass("div", "prod-edit");
+    let cartBtn = createteElWithClass("button", "btn btn-cart");
+    let cartIc = createteElWithClass("i", "fas fa-cart-plus");
+    cartBtn.innerText = "Добавить в корзину";
+    cartBtn.appendChild(cartIc);
+
+    let selectHolder = createteElWithClass("div", "ingr-select");
+    let select = document.createElement("select");
+    let optionDef = document.createElement("option");
+    optionDef.setAttribute("selected", "true");
+    optionDef.setAttribute("disabled", "disabled");
+    optionDef.innerText = "Выберете ингридиент";
+    select.append(optionDef);
+
+
+    for (let i = 0; i < elsForFiltering.length; i++) {
+      let option = document.createElement("option");
+      option.setAttribute("value", elsForFiltering[i]);
+      option.innerText = elsForFiltering[i];
+      select.append(option);
+    }
+
+    selectHolder.append(select);
+    addIngr.append(selectHolder);
+
     let prodDescr = createteElWithClass("div", "prod-descr");
 
     for (let j = 0; j < arr[i].ingredients.length; j++) {
       let prodIngr = createteElWithClass("span", "ingr");
-      prodIngr.innerHTML = arr[i].ingredients[j] + ", ";
+      prodIngr.innerHTML = arr[i].ingredients[j];
+      let comma = ", ";
+
       if (j === arr[i].ingredients.length - 1) {
-        prodIngr.innerHTML = arr[i].ingredients[j];
+        prodDescr.append(prodIngr);
+      } else {
+        prodDescr.append(prodIngr, comma);
       }
-      prodDescr.append(prodIngr);
     }
 
+    let prodKkal = createteElWithClass("div", "prod-kkal");
+    prodKkal.textContent = arr[i].calories + " кал.";
     let prodPrice = createteElWithClass("div", "prod-price");
-    prodPrice.innerText = arr[i].price;
-    prodProp.append(prodName, prodDescr, prodKkal, prodPrice);
+    prodPrice.innerText = arr[i].price + " грн";
+
+    let flexContainer = createteElWithClass("div", "flex-container");
+    flexContainer.append(prodPrice, cartBtn);
+    prodProp.append(prodName, editBtn, prodDescr, addIngr, prodKkal, flexContainer);
     prodHolder.append(prodProp, prodImg);
     col.appendChild(prodHolder);
     row.appendChild(col);
     ViewCol.append(row);
 
+    select.addEventListener("click", function (event) {
+      event.stopPropagation();
+    });
 
-    // prodName.addEventListener("click", function () {
-    //   let parent = this.parentElement;
-    //   parent.parentElement.classList.add("rotate");
-    // });
+    select.addEventListener("change", function (event) {
+      event.stopPropagation();
+      let newIngr = createteElWithClass("span", "ingr edit");
+      let comma = ", ";
+      newIngr.innerText = this.value;
+      prodDescr.append(comma, newIngr);
 
-    // prodImg.addEventListener("click", function () {
-    //   let parent = this.parentElement;
-    //   parent.classList.remove("rotate");
-    // });
 
-    prodHolder.onclick = function (event) {
-      let span = event.target.closest("span");
-      let name = event.target.closest(".prod-name");
-      let image = event.target.closest(".prod-img");
-      if (span) {
-        span.classList.toggle("linethrough");
-        console.log(span.innerHTML);
+      let editIngr = prodDescr.getElementsByClassName("ingr");
+      let pizzaName = arr[i].name;
+      let prodPrice = prodProp.querySelector(".prod-price");
+      let prodCal = prodProp.querySelector(".prod-kkal");
+
+      if (edit) {
+        recalc(editIngr, pizzaName, prodPrice, prodCal);
       }
-      if (name) {
-        this.classList.add("rotate");
-      }
-      if (image) {
-        this.classList.remove("rotate");
-      }
-    }
 
+      newIngr.addEventListener("click", function (event) {
+        event.stopPropagation();
+        newIngr.classList.toggle("linethrough");
+
+        if (edit) {
+          recalc(editIngr, pizzaName, prodPrice, prodCal);
+        }
+      })
+
+    });
 
   }
   return ViewCol;
 }
+
+function rotateProd() {
+  let prodHolder = document.querySelectorAll(".prod-holder");
+  prodHolder.forEach(function (el) {
+    el.addEventListener("click", function () {
+      this.classList.toggle("rotate");
+    });
+  })
+}
+let edit = false;
+function editBtnListener() {
+  edit = true;
+  let editBtn = document.querySelectorAll(".btn-edit");
+  editBtn.forEach(function (el) {
+    el.addEventListener("click", function (event) {
+      event.stopPropagation();
+      this.classList.toggle("active");
+      let parent = this.parentNode;
+      parent.classList.toggle("edit");
+
+      let editIngr = parent.getElementsByClassName("ingr");
+      let pizzaName = parent.querySelector(".prod-name > a").innerText;
+      let prodPrice = parent.querySelector(".prod-price");
+      let prodCal = parent.querySelector(".prod-kkal");
+
+      let spans = this.parentNode.querySelectorAll(".ingr");
+
+      spans.forEach(function (el) {
+
+        el.addEventListener("click", function (e) {
+          e.stopPropagation();
+          el.classList.toggle("linethrough");
+
+          recalc(editIngr, pizzaName, prodPrice, prodCal);
+
+        })
+
+      });
+
+
+    });
+  })
+}
+
+
+function recalc(arr, name, prodprice, prodCal) {
+
+  let price = 0;
+  let cal = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!(arr[i].className.includes("linethrough"))) {
+      ingrsPriceAndCal.forEach(function (val, key) {
+        let pizIngrHTML = arr[i].innerText.toLocaleLowerCase();
+        if (pizIngrHTML == key) {
+          let pizIngrPrice = val.price;
+          let pizCal = val.cal;
+          price += pizIngrPrice;
+          cal += pizCal;
+        }
+      });
+    }
+  }
+  if (name.includes("метровая") || name.includes("XXL")) {
+    price *= 2;
+    cal *= 2;
+  }
+
+  prodprice.innerText = price + " грн";
+  prodCal.textContent = cal + " кал."
+
+
+
+
+};
 
 function generateSelectSort() {
   let selectWrap = createteElWithClass("div", "select-wrap");
